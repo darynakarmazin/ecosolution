ec12550aa6fc7c91662eb6b1ef18a3a83336ae00
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-01 23:46:55 +0200

    Added  basic logic and markup for Electricity component.

--- src/components/Electricity/Electricity.jsx
+++ src/components/Electricity/Electricity.jsx
@@ -1,5 +1,82 @@
+import React, { useEffect } from 'react';
+import {
+  ElectricityAmount,
+  ElectricitySection,
+  ElectricityTitle,
+} from './Electricity.styles';
+
 function Electricity() {
-  return <div>Electricity</div>;
+  useEffect(() => {
+    function digitsCountersInit(digitsCountersItems) {
+      let digitsCounters = digitsCountersItems
+        ? digitsCountersItems
+        : document.querySelectorAll('[data-digits-couter]');
+      if (digitsCounters) {
+        digitsCounters.forEach(digitsCounter => {
+          digitsCountersAnimate(digitsCounter);
+        });
+      }
+    }
+
+    function digitsCountersAnimate(digitsCounter) {
+      let startTimestamp = null;
+      const duration = parseInt(digitsCounter.dataset.digitsCounter)
+        ? parseInt(digitsCounter.dataset.digitsCounter)
+        : 1000;
+      const startValue = parseInt(digitsCounter.dataset.max);
+      const startPosition = 0;
+      const step = timestamp => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        const value = Math.floor(progress * (startPosition + startValue));
+        digitsCounter.innerHTML = addSeparatorsToNumber(value);
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    }
+
+    function addSeparatorsToNumber(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    }
+
+    let options = {
+      threshold: 0.75,
+    };
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const targetElement = entry.target;
+          const digitsCountersItems = targetElement.querySelectorAll(
+            '[data-digits-couter]'
+          );
+          if (digitsCountersItems.length) {
+            digitsCountersInit(digitsCountersItems);
+          }
+        }
+      });
+    }, options);
+
+    let sections = document.querySelectorAll('.amount-item-numbers');
+    if (sections.length) {
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    }
+  }, []);
+
+  return (
+    <ElectricitySection className="amount-item-numbers">
+      <ElectricityTitle>Electricity we produced for all time</ElectricityTitle>
+      <ElectricityAmount>
+        <span data-digits-couter data-max="1134147814">
+          1.134.147.814
+        </span>
+        kWh
+      </ElectricityAmount>
+    </ElectricitySection>
+  );
 }
 
 export default Electricity;
---
+++ src/components/Electricity/Electricity.styles.jsx
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const ElectricitySection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-bottom: 120px;
+  }
+`;
+
+export const ElectricityTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 1;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 96px;
+  position: relative;
+
+  &:before {
+    content: '';
+    width: 1px;
+    height: 48px;
+    background: #97d28b;
+    display: block;
+    position: absolute;
+    left: 50%;
+    bottom: -96px;
+    transform: translateY(-50%);
+
+    @media screen and (min-width: 768px) {
+      height: 87px;
+      bottom: -159px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      bottom: -147px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+    width: 386px;
+    margin-bottom: 135px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    width: 491px;
+    margin-bottom: 120px;
+  }
+`;
+
+export const ElectricityAmount = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: var(--main-font-color);
+  font-family: var(--title-font);
+  text-align: center;
+  font-size: 24px;
+  line-height: 1;
+
+  @media screen and (min-width: 768px) {
+    gap: 24px;
+    font-size: 28px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+  }
+
+  span {
+    color: var(--hover-items-color);
+    text-align: center;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 1;
+    text-transform: uppercase;
+
+    @media screen and (min-width: 768px) {
+      font-size: 100px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      font-size: 164px;
+    }
+  }
+`;
