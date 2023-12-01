cba82c9abc4fa578394228efad03bed439d3832a
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-01 23:55:25 +0200

    Updated counter.

--- src/components/Electricity/Electricity.jsx
+++ src/components/Electricity/Electricity.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ElectricityAmount,
   ElectricitySection,
@@ -6,72 +6,29 @@ import {
 } from './Electricity.styles';
 
 function Electricity() {
+  const startCounterValue = 1134147814;
+  const [counterValue, setCounterValue] = useState(startCounterValue);
+
   useEffect(() => {
-    function digitsCountersInit(digitsCountersItems) {
-      let digitsCounters = digitsCountersItems
-        ? digitsCountersItems
-        : document.querySelectorAll('[data-digits-couter]');
-      if (digitsCounters) {
-        digitsCounters.forEach(digitsCounter => {
-          digitsCountersAnimate(digitsCounter);
-        });
-      }
-    }
+    const interval = setInterval(() => {
+      setCounterValue(prevValue => prevValue + 1);
+    }, 1000);
 
-    function digitsCountersAnimate(digitsCounter) {
-      let startTimestamp = null;
-      const duration = parseInt(digitsCounter.dataset.digitsCounter)
-        ? parseInt(digitsCounter.dataset.digitsCounter)
-        : 1000;
-      const startValue = parseInt(digitsCounter.dataset.max);
-      const startPosition = 0;
-      const step = timestamp => {
-        if (!startTimestamp) startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        const value = Math.floor(progress * (startPosition + startValue));
-        digitsCounter.innerHTML = addSeparatorsToNumber(value);
-        if (progress < 1) {
-          window.requestAnimationFrame(step);
-        }
-      };
-      window.requestAnimationFrame(step);
-    }
-
-    function addSeparatorsToNumber(value) {
-      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-    }
-
-    let options = {
-      threshold: 0.75,
+    return () => {
+      clearInterval(interval);
     };
-    let observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const targetElement = entry.target;
-          const digitsCountersItems = targetElement.querySelectorAll(
-            '[data-digits-couter]'
-          );
-          if (digitsCountersItems.length) {
-            digitsCountersInit(digitsCountersItems);
-          }
-        }
-      });
-    }, options);
-
-    let sections = document.querySelectorAll('.amount-item-numbers');
-    if (sections.length) {
-      sections.forEach(section => {
-        observer.observe(section);
-      });
-    }
   }, []);
 
+  const lastThreeDigitsString = (counterValue % 1000)
+    .toString()
+    .padStart(3, '0');
+
   return (
-    <ElectricitySection className="amount-item-numbers">
+    <ElectricitySection>
       <ElectricityTitle>Electricity we produced for all time</ElectricityTitle>
       <ElectricityAmount>
-        <span data-digits-couter data-max="1134147814">
-          1.134.147.814
+        <span data-digits-couter data-max={counterValue}>
+          1.134.147.{lastThreeDigitsString}
         </span>
         kWh
       </ElectricityAmount>
