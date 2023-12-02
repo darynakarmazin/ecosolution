487d27831896e16490aa87827cd900aae43c88d7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-02 22:58:47 +0200

    Updated styles for FAQ component.

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react';
 import { ReactComponent as AddIcon } from '../../img/faq/add.svg';
 import { ReactComponent as MinusIcon } from '../../img/faq/minus.svg';
-import { TouchLink } from '../Header/Header.styled';
 import ArrowIcon from '../../img/arrow.svg';
+import {
+  FaqAddQues,
+  FaqAddinionalLink,
+  FaqAddinionalWrapper,
+  FaqAnsver,
+  FaqButton,
+  FaqList,
+  FaqListItem,
+  FaqQuestion,
+  FaqQuestionWrapper,
+  FaqSection,
+  FaqTitle,
+  FaqTitleTab,
+  TouchLink,
+} from './FAQ.styled';
 
 const faqData = [
   {
@@ -14,22 +28,26 @@ const faqData = [
   {
     question:
       "What sets EcoSolution's renewable energy solutions apart from others on the market?",
-    answer: 'Answer for the second question goes here.',
+    answer:
+      'Wind turbines and solar panels generate electricity through different mechanisms. Wind turbines harness the kinetic energy of the wind to turn blades, which then drive a generator. Solar panels convert sunlight into electricity through the photovoltaic effect. When integrated into a renewable energy system, these technologies complement each other by providing a continuous and reliable power supply. Wind power is often more abundant during certain times, while solar power is consistent during daylight hours, resulting in a more stable overall energy output.',
   },
   {
     question:
       'How can businesses and communities benefit from integrating renewable energy solutions from EcoSolution?',
-    answer: 'Answer for the third question goes here.',
+    answer:
+      'Wind turbines and solar panels generate electricity through different mechanisms. Wind turbines harness the kinetic energy of the wind to turn blades, which then drive a generator. Solar panels convert sunlight into electricity through the photovoltaic effect. When integrated into a renewable energy system, these technologies complement each other by providing a continuous and reliable power supply. Wind power is often more abundant during certain times, while solar power is consistent during daylight hours, resulting in a more stable overall energy output.',
   },
   {
     question:
       'What measures does EcoSolution take to ensure the environmental sustainability of its products?',
-    answer: 'Answer for the fourth question goes here.',
+    answer:
+      'Wind turbines and solar panels generate electricity through different mechanisms. Wind turbines harness the kinetic energy of the wind to turn blades, which then drive a generator. Solar panels convert sunlight into electricity through the photovoltaic effect. When integrated into a renewable energy system, these technologies complement each other by providing a continuous and reliable power supply. Wind power is often more abundant during certain times, while solar power is consistent during daylight hours, resulting in a more stable overall energy output.',
   },
   {
     question:
       'How does EcoSolution engage with local communities and support a just transition to renewable energy?',
-    answer: 'Answer for the fifth question goes here.',
+    answer:
+      'Wind turbines and solar panels generate electricity through different mechanisms. Wind turbines harness the kinetic energy of the wind to turn blades, which then drive a generator. Solar panels convert sunlight into electricity through the photovoltaic effect. When integrated into a renewable energy system, these technologies complement each other by providing a continuous and reliable power supply. Wind power is often more abundant during certain times, while solar power is consistent during daylight hours, resulting in a more stable overall energy output.',
   },
 ];
 
@@ -41,34 +59,40 @@ function FAQ() {
   };
 
   return (
-    <section>
-      <h2>Frequently Asked Questions</h2>
-      <ul>
-        {faqData.map((item, index) => (
-          <li key={index}>
-            <div>
-              <p>{item.question}</p>
-              <button
-                type="button"
-                aria-label="Toggle answer"
-                onClick={() => handleToggle(index)}
-              >
-                {openIndex === index ? <MinusIcon /> : <AddIcon />}
-              </button>
-            </div>
-            {openIndex === index && <p>{item.answer}</p>}
-          </li>
-        ))}
-      </ul>
+    <FaqSection>
+      <FaqTitle>Frequently Asked Questions</FaqTitle>
 
-      <p>Didn't find the answer to your question? </p>
-      <TouchLink href="#">
-        Contact Us
-        <div>
-          <img src={ArrowIcon} alt="arrow icon" />
-        </div>
-      </TouchLink>
-    </section>
+      <FaqList>
+        {faqData.map((item, index) => (
+          <FaqListItem key={index}>
+            <FaqButton
+              type="button"
+              aria-label="Toggle answer"
+              onClick={() => handleToggle(index)}
+            >
+              {openIndex === index ? <MinusIcon /> : <AddIcon />}
+            </FaqButton>
+            <FaqQuestionWrapper>
+              <FaqQuestion>{item.question}</FaqQuestion>
+              {openIndex === index && <FaqAnsver>{item.answer}</FaqAnsver>}
+            </FaqQuestionWrapper>
+          </FaqListItem>
+        ))}
+      </FaqList>
+
+      <FaqAddinionalWrapper>
+        <FaqTitleTab>Frequently Asked Questions</FaqTitleTab>
+        <FaqAddinionalLink>
+          <FaqAddQues>Didn't find the answer to your question? </FaqAddQues>
+          <TouchLink href="#">
+            Contact Us
+            <div>
+              <img src={ArrowIcon} alt="arrow icon" />
+            </div>
+          </TouchLink>
+        </FaqAddinionalLink>
+      </FaqAddinionalWrapper>
+    </FaqSection>
   );
 }
 
---
+++ src/components/FAQ/FAQ.styled.jsx
@@ -0,0 +1,209 @@
+import styled from 'styled-components';
+
+export const FaqSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    gap: 24px;
+    flex-direction: row;
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 185px;
+    padding-bottom: 124px;
+  }
+`;
+
+export const FaqTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 1;
+  text-transform: uppercase;
+  margin-bottom: 24px;
+  position: relative;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+    font-size: 36px;
+    width: 347px;
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    width: 398px;
+  }
+`;
+
+export const FaqTitleTab = styled.h2`
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    font-size: 36px;
+    width: 347px;
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    width: 398px;
+  }
+`;
+
+export const FaqList = styled.ul`
+  margin-bottom: 16px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0;
+    width: 342px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 596px;
+  }
+`;
+
+export const FaqListItem = styled.li`
+  display: flex;
+  gap: 8px;
+  align-items: flex-start;
+  justify-content: center;
+  border-top: 1px solid var(--hover-items-color);
+  padding: 16px 0;
+
+  @media screen and (min-width: 768px) {
+    gap: 16px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 24px;
+  }
+`;
+
+export const FaqButton = styled.button`
+  border: none;
+  background: transparent;
+  margin-top: 21px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const FaqQuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 24px;
+  }
+`;
+
+export const FaqQuestion = styled.p`
+  text-align: justify;
+  font-size: 18px;
+  letter-spacing: -0.72px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 24px;
+    letter-spacing: -0.96px;
+  }
+`;
+
+export const FaqAnsver = styled.p`
+  text-align: justify;
+  font-size: 14px;
+  letter-spacing: -0.56px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 16px;
+    letter-spacing: -0.64px;
+  }
+`;
+
+export const FaqAddinionalWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`;
+
+export const FaqAddinionalLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    align-items: flex-start;
+  }
+
+  @media screen and (min-width: 1280px) {
+    align-items: center;
+  }
+`;
+
+export const TouchLink = styled.a`
+  display: inline-flex;
+  padding: 10px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  border-radius: 500px;
+  background: var(--hover-items-color, #97d28b);
+  color: var(--main-font-color, #173d33);
+  font-family: var(--main-font);
+  font-size: 16px;
+  letter-spacing: -0.64px;
+  transition: var(--transition-dur-and-func);
+
+  div {
+    width: 14px;
+    height: 14px;
+    background: #173d33;
+    border-radius: 50%;
+    transition: var(--transition-dur-and-func);
+  }
+
+  &:hover,
+  :focus {
+    background: #173d33;
+    color: #97d28b;
+
+    div {
+      background: #97d28b;
+    }
+  }
+`;
+
+export const FaqAddQues = styled.p`
+  font-size: 18px;
+  letter-spacing: -0.72px;
+  text-align: center;
+  margin-bottom: 12px;
+
+  @media screen and (min-width: 768px) {
+    align-items: flex-start;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 24px;
+    letter-spacing: -0.96px;
+  }
+`;
