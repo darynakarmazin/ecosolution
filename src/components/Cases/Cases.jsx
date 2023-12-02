29a64f95d47e3b964a3c33d72726980d7bf2fe55
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-02 12:12:52 +0200

    Added list and cads markup for Cases components.

--- src/components/Cases/Cases.jsx
+++ src/components/Cases/Cases.jsx
@@ -1,5 +1,272 @@
+import ZakhidnyiBuhImg1x from '../../img/cases/zakhidnyi-buh.jpg';
+import ZakhidnyiBuhImg2x from '../../img/cases/zakhidnyi-buh@2x.jpg';
+import BoschImg1x from '../../img/cases/bosch.jpg';
+import BoschImg2x from '../../img/cases/bosch@2x.jpg';
+import BiotechImg1x from '../../img/cases/biotech.jpg';
+import BiotechImg2x from '../../img/cases/biotech@2x.jpg';
+import HealthyFarmImg1x from '../../img/cases/healthy-farm.jpg';
+import HealthyFarmImg2x from '../../img/cases/healthy-farm@2x.jpg';
+import EnterpriseBiotechImg1x from '../../img/cases/enterprise-biotech.jpg';
+import EnterpriseBiotechImg2x from '../../img/cases/enterprise-biotech@2x.jpg';
+import {
+  CasesBottomInfo,
+  CasesDescription,
+  CasesItem,
+  CasesItemWrapper,
+  CasesList,
+  CasesSection,
+  CasesTitle,
+} from './Cases.styles';
+
+import { ReactComponent as ArrowTOPRight } from '../../img/cases/arrow-right.svg';
+
 function Cases() {
-  return <div id="cases">Cases</div>;
+  return (
+    <CasesSection>
+      <CasesTitle>Successful cases of our company</CasesTitle>
+      <div>
+        <span>01 / 05</span>
+        <div>
+          <button>prev</button>
+          <button>next</button>
+        </div>
+      </div>
+
+      <CasesList>
+        <li>
+          <CasesItem>
+            <picture>
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`
+          ${ZakhidnyiBuhImg1x} 1x,
+          ${ZakhidnyiBuhImg2x} 2x
+        `}
+                width="596"
+                height="296px"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`
+          ${ZakhidnyiBuhImg1x} 1x,
+          ${ZakhidnyiBuhImg2x} 2x
+        `}
+                width="342"
+                height="168"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`
+          ${ZakhidnyiBuhImg1x} 1x,
+          ${ZakhidnyiBuhImg2x} 2x
+        `}
+              />
+              <img src={ZakhidnyiBuhImg1x} alt="Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”" />
+            </picture>
+            <CasesItemWrapper>
+              <CasesDescription>
+                <p>
+                  Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”
+                </p>
+                <button>
+                  <ArrowTOPRight />
+                </button>
+              </CasesDescription>
+              <CasesBottomInfo>
+                <p>Wind Power for auto field irrigation</p>
+                <p>July 2023</p>
+              </CasesBottomInfo>
+            </CasesItemWrapper>
+          </CasesItem>
+        </li>
+        <li>
+          <CasesItem>
+            <picture>
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`
+          ${BoschImg1x} 1x,
+          ${BoschImg2x} 2x
+        `}
+                width="596"
+                height="296px"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`
+          ${BoschImg1x} 1x,
+          ${BoschImg2x} 2x
+        `}
+                width="342"
+                height="168"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`
+          ${BoschImg1x} 1x,
+          ${BoschImg2x} 2x
+        `}
+              />
+              <img src={BoschImg1x} alt="Zhytomyr city Private Enterprise “Bosch”" />
+            </picture>
+            <CasesItemWrapper>
+              <CasesDescription>
+                <p>
+                Zhytomyr city Private Enterprise “Bosch”
+                </p>
+                <button>
+                  <ArrowTOPRight />
+                </button>
+              </CasesDescription>
+              <CasesBottomInfo>
+                <p>Solar Panels for industrial use</p>
+                <p>November 2023</p>
+              </CasesBottomInfo>
+            </CasesItemWrapper>
+          </CasesItem>
+        </li>
+        <li>
+          <CasesItem>
+            <picture>
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`
+          ${BiotechImg1x} 1x,
+          ${BiotechImg2x} 2x
+        `}
+                width="596"
+                height="296px"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`
+          ${BiotechImg1x} 1x,
+          ${BiotechImg2x} 2x
+        `}
+                width="342"
+                height="168"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`
+          ${BiotechImg1x} 1x,
+          ${BiotechImg2x} 2x
+        `}
+              />
+              <img src={BiotechImg1x} alt="Rivne city Private Enterprise “Biotech”" />
+            </picture>
+            <CasesItemWrapper>
+              <CasesDescription>
+                <p>
+                Rivne city Private Enterprise “Biotech”
+                </p>
+                <button>
+                  <ArrowTOPRight />
+                </button>
+              </CasesDescription>
+              <CasesBottomInfo>
+                <p>Thermal modules</p>
+                <p>Thermal modules3</p>
+              </CasesBottomInfo>
+            </CasesItemWrapper>
+          </CasesItem>
+        </li>
+        <li>
+          <CasesItem>
+            <picture>
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`
+          ${HealthyFarmImg1x} 1x,
+          ${HealthyFarmImg2x} 2x
+        `}
+                width="596"
+                height="296px"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`
+          ${HealthyFarmImg1x} 1x,
+          ${HealthyFarmImg2x} 2x
+        `}
+                width="342"
+                height="168"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`
+          ${HealthyFarmImg1x} 1x,
+          ${HealthyFarmImg2x} 2x
+        `}
+              />
+              <img src={HealthyFarmImg1x} alt="Kherson city Private Enterprise “HealthyFarm”" />
+            </picture>
+            <CasesItemWrapper>
+              <CasesDescription>
+                <p>
+                Kherson city Private Enterprise “HealthyFarm”
+                </p>
+                <button>
+                  <ArrowTOPRight />
+                </button>
+              </CasesDescription>
+              <CasesBottomInfo>
+                <p>Wind power</p>
+                <p>September 2021</p>
+              </CasesBottomInfo>
+            </CasesItemWrapper>
+          </CasesItem>
+        </li>
+        <li>
+          <CasesItem>
+            <picture>
+              <source
+                media="(min-width: 1280px)"
+                srcSet={`
+          ${EnterpriseBiotechImg1x} 1x,
+          ${EnterpriseBiotechImg2x} 2x
+        `}
+                width="596"
+                height="296px"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`
+          ${EnterpriseBiotechImg1x} 1x,
+          ${EnterpriseBiotechImg2x} 2x
+        `}
+                width="342"
+                height="168"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`
+          ${EnterpriseBiotechImg1x} 1x,
+          ${EnterpriseBiotechImg2x} 2x
+        `}
+              />
+              <img src={EnterpriseBiotechImg1x} alt="Zaporizhia city Private Enterprise “Biotech”" />
+            </picture>
+            <CasesItemWrapper>
+              <CasesDescription>
+                <p>
+                Zaporizhia city Private Enterprise “Biotech”
+                </p>
+                <button>
+                  <ArrowTOPRight />
+                </button>
+              </CasesDescription>
+              <CasesBottomInfo>
+                <p>Mini nuclear stations</p>
+                <p>May 2021</p>
+              </CasesBottomInfo>
+            </CasesItemWrapper>
+          </CasesItem>
+        </li>
+
+      </CasesList>
+    </CasesSection>
+  );
 }
 
 export default Cases;
---
+++ src/components/Cases/Cases.styles.jsx
@@ -0,0 +1,162 @@
+import styled from 'styled-components';
+
+export const CasesSection = styled.section`
+  padding-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-bottom: 124px;
+  }
+`;
+
+export const CasesTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 1;
+  text-transform: uppercase;
+  margin-bottom: 24px;
+  position: relative;
+
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+    width: 264px;
+    margin-bottom: 38px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    width: 398px;
+    margin-bottom: 120px;
+  }
+`;
+
+export const CasesList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 48px;
+  }
+`;
+
+export const CasesItem = styled.div`
+  max-width: 100%;
+  height: auto;
+  background: #eaedf1;
+
+  @media screen and (min-width: 768px) {
+    width: 342px;
+    height: 322px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 596px;
+    height: 506px;
+  }
+
+  @media screen and (max-width: 767px) {
+    img {
+      width: 440px;
+      max-width: 100%;
+      height: auto;
+    }
+  }
+`;
+
+export const CasesItemWrapper = styled.div`
+  padding: 24px 12px 12px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 38px 48px;
+  }
+`;
+
+export const CasesDescription = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 87px;
+  border-bottom: 1px solid var(--hover-items-color);
+  margin-bottom: 12px;
+
+  @media screen and (min-width: 768px) {
+    height: 88px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    height: 92px;
+    margin-bottom: 24px;
+  }
+
+  p {
+    width: 175px;
+    font-size: 18px;
+    letter-spacing: -0.72px;
+
+    @media screen and (min-width: 768px) {
+      width: 230px;
+      font-size: 20px;
+      letter-spacing: -0.8px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      width: 261px;
+      font-size: 24px;
+      letter-spacing: -0.96px;
+    }
+  }
+
+  button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
+    height: 60px;
+    padding: 16px;
+    gap: 10px;
+    border: none;
+    border-radius: 100px;
+    background: #97d28b;
+    transition: var(--transition-dur-and-func);
+
+    img {
+      width: 28px;
+      height: 28px;
+      transition: var(--transition-dur-and-func);
+    }
+
+    &:hover,
+    :focus {
+      background: #173d33;
+      svg path {
+  stroke: #97d28b; 
+    }
+  }
+`;
+
+export const CasesBottomInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    font-size: 12px;
+    letter-spacing: -0.48px;
+
+    @media screen and (min-width: 768px) {
+        font-size: 14px;
+      }
+    
+      @media screen and (min-width: 1280px) {
+        font-size: 16px;
+      }
+  }
+`;
