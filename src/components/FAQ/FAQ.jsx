99d661de64f61db3b0e6b130e4a34764d350383e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-02 17:47:17 +0200

    Updated json for questions.

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -2,157 +2,61 @@ import { useState } from 'react';
 import { ReactComponent as AddIcon } from '../../img/faq/add.svg';
 import { ReactComponent as MinusIcon } from '../../img/faq/minus.svg';
 
-function FAQ() {
-  const [isOpen, setIsOpen] = useState(false);
+const faqData = [
+  {
+    question:
+      'How do wind turbines and solar panels work together in a renewable energy system?',
+    answer:
+      'Wind turbines and solar panels generate electricity through different mechanisms. Wind turbines harness the kinetic energy of the wind to turn blades, which then drive a generator. Solar panels convert sunlight into electricity through the photovoltaic effect. When integrated into a renewable energy system, these technologies complement each other by providing a continuous and reliable power supply. Wind power is often more abundant during certain times, while solar power is consistent during daylight hours, resulting in a more stable overall energy output.',
+  },
+  {
+    question:
+      "What sets EcoSolution's renewable energy solutions apart from others on the market?",
+    answer: 'Answer for the second question goes here.',
+  },
+  {
+    question:
+      'How can businesses and communities benefit from integrating renewable energy solutions from EcoSolution?',
+    answer: 'Answer for the third question goes here.',
+  },
+  {
+    question:
+      'What measures does EcoSolution take to ensure the environmental sustainability of its products?',
+    answer: 'Answer for the fourth question goes here.',
+  },
+  {
+    question:
+      'How does EcoSolution engage with local communities and support a just transition to renewable energy?',
+    answer: 'Answer for the fifth question goes here.',
+  },
+];
 
-  const handleToggles = () => {
-    setIsOpen(!isOpen);
+function FAQ() {
+  const [openIndex, setOpenIndex] = useState(0);
+
+  const handleToggle = index => {
+    setOpenIndex(prevIndex => (prevIndex === index ? -1 : index));
   };
 
   return (
     <section>
       <h2>Frequently Asked Questions</h2>
       <ul>
-        <li>
-          <div>
-            <p>
-              How do wind turbines and solar panels work together in a renewable
-              energy system?
-            </p>
-            <button
-              type="button"
-              aria-label="Open answer"
-              onClick={handleToggles}
-            >
-              {isOpen ? <MinusIcon /> : <AddIcon />}
-            </button>
-          </div>
-          {isOpen && (
-            <p>
-              Wind turbines and solar panels generate electricity through
-              different mechanisms. Wind turbines harness the kinetic energy of
-              the wind to turn blades, which then drive a generator. Solar
-              panels convert sunlight into electricity through the photovoltaic
-              effect. When integrated into a renewable energy system, these
-              technologies complement each other by providing a continuous and
-              reliable power supply. Wind power is often more abundant during
-              certain times, while solar power is consistent during daylight
-              hours, resulting in a more stable overall energy output.
-            </p>
-          )}
-        </li>
-        <li>
-          <div>
-            <p>
-              What sets EcoSolution's renewable energy solutions apart from
-              others on the market?
-            </p>
-            <button
-              type="button"
-              aria-label="Open answer"
-              onClick={handleToggles}
-            >
-              {isOpen ? <MinusIcon /> : <AddIcon />}
-            </button>
-          </div>
-          {isOpen && (
-            <p>
-              Wind turbines and solar panels generate electricity through
-              different mechanisms. Wind turbines harness the kinetic energy of
-              the wind to turn blades, which then drive a generator. Solar
-              panels convert sunlight into electricity through the photovoltaic
-              effect. When integrated into a renewable energy system, these
-              technologies complement each other by providing a continuous and
-              reliable power supply. Wind power is often more abundant during
-              certain times, while solar power is consistent during daylight
-              hours, resulting in a more stable overall energy output.
-            </p>
-          )}
-        </li>
-        <li>
-          <div>
-            <p>
-              How can businesses and communities benefit from integrating
-              renewable energy solutions from EcoSolution?
-            </p>
-            <button
-              type="button"
-              aria-label="Open answer"
-              onClick={handleToggles}
-            >
-              {isOpen ? <MinusIcon /> : <AddIcon />}
-            </button>
-          </div>
-          {isOpen && (
-            <p>
-              Wind turbines and solar panels generate electricity through
-              different mechanisms. Wind turbines harness the kinetic energy of
-              the wind to turn blades, which then drive a generator. Solar
-              panels convert sunlight into electricity through the photovoltaic
-              effect. When integrated into a renewable energy system, these
-              technologies complement each other by providing a continuous and
-              reliable power supply. Wind power is often more abundant during
-              certain times, while solar power is consistent during daylight
-              hours, resulting in a more stable overall energy output.
-            </p>
-          )}
-        </li>
-        <li>
-          <div>
-            <p>
-              What measures does EcoSolution take to ensure the environmental
-              sustainability of its products?
-            </p>
-            <button
-              type="button"
-              aria-label="Open answer"
-              onClick={handleToggles}
-            >
-              {isOpen ? <MinusIcon /> : <AddIcon />}
-            </button>
-          </div>
-          {isOpen && (
-            <p>
-              Wind turbines and solar panels generate electricity through
-              different mechanisms. Wind turbines harness the kinetic energy of
-              the wind to turn blades, which then drive a generator. Solar
-              panels convert sunlight into electricity through the photovoltaic
-              effect. When integrated into a renewable energy system, these
-              technologies complement each other by providing a continuous and
-              reliable power supply. Wind power is often more abundant during
-              certain times, while solar power is consistent during daylight
-              hours, resulting in a more stable overall energy output.
-            </p>
-          )}
-        </li>
-        <li>
-          <div>
-            <p>
-              How does EcoSolution engage with local communities and support a
-              just transition to renewable energy?
-            </p>
-            <button
-              type="button"
-              aria-label="Open answer"
-              onClick={handleToggles}
-            >
-              {isOpen ? <MinusIcon /> : <AddIcon />}
-            </button>
-          </div>
-          {isOpen && (
-            <p>
-              Wind turbines and solar panels generate electricity through
-              different mechanisms. Wind turbines harness the kinetic energy of
-              the wind to turn blades, which then drive a generator. Solar
-              panels convert sunlight into electricity through the photovoltaic
-              effect. When integrated into a renewable energy system, these
-              technologies complement each other by providing a continuous and
-              reliable power supply. Wind power is often more abundant during
-              certain times, while solar power is consistent during daylight
-              hours, resulting in a more stable overall energy output.
-            </p>
-          )}
-        </li>
+        {faqData.map((item, index) => (
+          <li key={index}>
+            <div>
+              <p>{item.question}</p>
+              <button
+                type="button"
+                aria-label="Toggle answer"
+                onClick={() => handleToggle(index)}
+              >
+                {openIndex === index ? <MinusIcon /> : <AddIcon />}
+              </button>
+            </div>
+            {openIndex === index && <p>{item.answer}</p>}
+          </li>
+        ))}
       </ul>
     </section>
   );
