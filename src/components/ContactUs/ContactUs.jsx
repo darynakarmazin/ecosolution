ca2bf2dd6191747b510a2a0389119e7c8f4e3523
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-03 10:55:36 +0200

    Added contacts for ContactUs.jsx component.

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,5 +1,76 @@
+import { ReactComponent as Facebook } from '../../img/facebook.svg';
+import { ReactComponent as Instagram } from '../../img/instagram.svg';
+import { ReactComponent as Call } from '../../img/contact/call.svg';
+import { ReactComponent as Map } from '../../img/contact/map.svg';
+import { ReactComponent as Mail } from '../../img/contact/sms.svg';
+
+import {
+  ContactList,
+  ContactListItem,
+  ContactListTitle,
+  ContactRow,
+  ContactUsSection,
+  ContactUsTitle,
+} from './ContactUs.styled';
+
 function ContactUs() {
-  return <section id="contactUs">ContactUs</section>;
+  return (
+    <ContactUsSection id="contactUs">
+      <ContactUsTitle>Contact us</ContactUsTitle>
+      <ContactList>
+        <ContactListItem>
+          <ContactListTitle>Phone:</ContactListTitle>
+          <ContactRow>
+            <Call />
+            <a target="_blank" rel="noreferrer" href="tel:+380981234567">
+              38 (098) 12 34 567
+            </a>
+          </ContactRow>
+          <ContactRow>
+            <Call />
+            <a target="_blank" rel="noreferrer" href="tel:+380931234567">
+              38 (093) 12 34 567
+            </a>
+          </ContactRow>
+        </ContactListItem>
+        <ContactListItem>
+          <ContactListTitle>E-mail:</ContactListTitle>
+          <ContactRow>
+            <Mail />
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:office@ecosolution.com"
+            >
+              office@ecosolution.com
+            </a>
+          </ContactRow>
+        </ContactListItem>
+        <ContactListItem>
+          <ContactListTitle>Address:</ContactListTitle>
+          <ContactRow>
+            <Map />
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%A8%D0%BE%D1%82%D0%B0+%D0%A0%D1%83%D1%81%D1%82%D0%B0%D0%B2%D0%B5%D0%BB%D1%96,+7,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/data=!4m2!3m1!1s0x473add6785a9dce5:0x92b4b3ec68e99c14?sa=X&ved=2ahUKEwj8oun6s-6CAxXAQPEDHeG8CWYQ8gF6BAgQEAA"
+            >
+              79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
+            </a>
+          </ContactRow>
+        </ContactListItem>
+        <ContactListItem>
+          <ContactListTitle>Social Networks:</ContactListTitle>
+          <ContactRow>
+            <Facebook />
+            <Instagram />
+          </ContactRow>
+        </ContactListItem>
+      </ContactList>
+
+      <form action=""></form>
+    </ContactUsSection>
+  );
 }
 
 export default ContactUs;
---
+++ src/components/ContactUs/ContactUs.styled.jsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const ContactUsSection = styled.section`
+  padding-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-bottom: 120px;
+  }
+`;
+
+export const ContactUsTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 1;
+  text-transform: uppercase;
+  margin-bottom: 24px;
+  position: relative;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+    font-size: 36px;
+    width: 347px;
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    width: 398px;
+  }
+`;
+
+export const ContactList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const ContactListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const ContactListTitle = styled.p`
+  font-size: 16px;
+  letter-spacing: -0.64px;
+`;
+
+export const ContactRow = styled.div`
+  display: flex;
+  gap: 8px;
+  align-item: center;
+
+  a {
+    color: var(--main-font-color);
+    font-family: var(--main-font);
+    font-size: 20px;
+    letter-spacing: -0.8px;
+    max-width: 212px;
+  }
+`;
