75247d817b4df808d23542122e1c416d4b2c7398
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-04 12:28:41 +0200

    Refactor data stogares.

--- src/components/Cases/Cases.jsx
+++ src/components/Cases/Cases.jsx
@@ -2,16 +2,6 @@ import { useState } from 'react';
 import { ReactComponent as ArrowTOPRight } from '../../img/cases/arrow-top-right.svg';
 import { ReactComponent as ArrowLeft } from '../../img/cases/arrow-left.svg';
 import { ReactComponent as ArrowRight } from '../../img/cases/arrow-right.svg';
-import ZakhidnyiBuhImg1x from '../../img/cases/zakhidnyi-buh.jpg';
-import ZakhidnyiBuhImg2x from '../../img/cases/zakhidnyi-buh@2x.jpg';
-import BoschImg1x from '../../img/cases/bosch.jpg';
-import BoschImg2x from '../../img/cases/bosch@2x.jpg';
-import BiotechImg1x from '../../img/cases/biotech.jpg';
-import BiotechImg2x from '../../img/cases/biotech@2x.jpg';
-import HealthyFarmImg1x from '../../img/cases/healthy-farm.jpg';
-import HealthyFarmImg2x from '../../img/cases/healthy-farm@2x.jpg';
-import EnterpriseBiotechImg1x from '../../img/cases/enterprise-biotech.jpg';
-import EnterpriseBiotechImg2x from '../../img/cases/enterprise-biotech@2x.jpg';
 import {
   Buttons,
   CasesBottomInfo,
@@ -34,62 +24,9 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css/navigation';
 
 import { useMediaQuery } from 'react-responsive';
+import { slides } from '../data/slides';
 
 function Cases() {
-  const slides = [
-    {
-      image1x: ZakhidnyiBuhImg1x,
-      image2x: ZakhidnyiBuhImg2x,
-      alt: 'Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”',
-      description:
-        'Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”',
-      info: {
-        title: 'Wind Power for auto field irrigation',
-        date: 'July 2023',
-      },
-    },
-    {
-      image1x: BoschImg1x,
-      image2x: BoschImg2x,
-      alt: 'Zhytomyr city Private Enterprise “Bosch”',
-      description: 'Zhytomyr city Private Enterprise “Bosch”',
-      info: {
-        title: 'Solar Panels for industrial use',
-        date: 'November 2023',
-      },
-    },
-    {
-      image1x: BiotechImg1x,
-      image2x: BiotechImg2x,
-      alt: 'Rivne city Private Enterprise “Biotech”',
-      description: 'Rivne city Private Enterprise “Biotech”',
-      info: {
-        title: 'Thermal modules',
-        date: 'Thermal modules3',
-      },
-    },
-    {
-      image1x: HealthyFarmImg1x,
-      image2x: HealthyFarmImg2x,
-      alt: 'Kherson city Private Enterprise “HealthyFarm”',
-      description: 'Kherson city Private Enterprise “HealthyFarm”',
-      info: {
-        title: 'Wind power',
-        date: 'September 2021',
-      },
-    },
-    {
-      image1x: EnterpriseBiotechImg1x,
-      image2x: EnterpriseBiotechImg2x,
-      alt: 'Zaporizhia city Private Enterprise “Biotech”',
-      description: 'Zaporizhia city Private Enterprise “Biotech”',
-      info: {
-        title: 'Mini nuclear stations',
-        date: 'May 2021',
-      },
-    },
-  ];
-
   const [count, setCount] = useState(1);
 
   const isMobile = useMediaQuery({ maxWidth: 767 });
